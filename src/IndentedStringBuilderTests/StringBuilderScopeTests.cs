737538cb2b18c12f70c6ac38b1b3a83a26b82922
Remigijus Pankevičius <r-pankevicius@users.noreply.github.com>
2024-11-05 18:18:15 +0200

    Added failing test InnerScope_Should_BeIntended

--- src/IndentedStringBuilderTests/StringBuilderScopeTests.cs
+++ src/IndentedStringBuilderTests/StringBuilderScopeTests.cs
@@ -8,10 +8,26 @@ public class StringBuilderScopeTests
     [Fact]
     public void OneLevelScope_Should_ActLikeStringBuilder()
     {
-        var sb = new StringBuilderScope();
+        using StringBuilderScope sb = new();
         sb.Append("abc");
         sb.Append("xyz");
         sb.ToString().Should().Be("abcxyz");
+    }
+
+    [Fact]
+    public void InnerScope_Should_BeIntended()
+    {
+        using StringBuilderScope sb = new();
+
+        sb.Append("for (var i = 0; i < array.length; i++)");
+
+        using (StringBuilderScope forBody = new(new(indent: "  ", blockStart: " {\n", blockEnd: "\n}\n")))
+        {
+            forBody.AppendLine("sum += array[i];");
+        }
+        sb.AppendLine("console.log(sum);");
+
+        sb.ToString().Should().Be("for (var i = 0; i < array.length; i++)\n {\n  sum += array[i];\n}\nconsole.log(sum);\n");
     }
 }
 
